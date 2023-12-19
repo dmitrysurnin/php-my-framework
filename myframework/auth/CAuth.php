6ff804ac480b106f17f6a716f5b8954d43b5801b
ds <dmitrysurnin@gmail.com>
2023-12-19 14:40:07 +0300

    cauth

---
+++ myframework/auth/CAuth.php
@@ -0,0 +1,203 @@
+<?php
+namespace myframework;
+
+/**
+ * Регистрация и авторизация
+ */
+abstract class CAuth extends CAuthShort
+{
+	/*
+	 * Регистрация пользователя по логину и email
+	 * Должен быть уникален логин и уникален email
+	 * Соответственно, логиниться можно или по связке логин + пароль, или по email + пароль
+	 */
+	public function registerByLoginEmail(string $login, string $email, string $password, ?string $captcha): bool
+	{
+		if ($this->errors = $this->_validateCaptcha($captcha, 'register'))
+		{
+			return false;
+		}
+
+		if ($this->errors = $this->_validateLogin($login) + $this->_validateEmail($email) + $this->_validatePassword($password))
+		{
+			return false;
+		}
+
+		if ($this->loadUserByLogin($login))
+		{
+			$this->errors['login'] = $this->_messages['login.exists']; /// такой логин уже зарегистрирован
+
+			return false;
+		}
+
+		if ($this->loadUserByEmail($email))
+		{
+			$this->errors['email'] = $this->_messages['email.exists_forgot']; /// такой email уже зарегистрирован
+
+			return false;
+		}
+
+		$this->_registerNewUserLoginEmail($login, $email, $password);
+
+		return true;
+	}
+
+	/*
+	 * Регистрация пользователя по имени (фио) и email
+	 * Должен быть уникален только email, а имя может быть неуникалным
+	 * Соответственно, логиниться можно только по связке email + пароль
+	 */
+	public function registerByNameEmail(string $name, string $email, string $password, ?string $captcha): bool
+	{
+		if ($this->errors = $this->_validateCaptcha($captcha, 'register'))
+		{
+			return false;
+		}
+
+		if ($this->errors = $this->_validateName($name) + $this->_validateEmail($email) + $this->_validatePassword($password))
+		{
+			return false;
+		}
+
+		if ($this->loadUserByEmail($email))
+		{
+			$this->errors['email'] = $this->_messages['email.exists_forgot']; /// такой email уже зарегистрирован
+
+			$this->_addCaptchaAction('register');
+
+			return false;
+		}
+
+		$this->_registerNewUserNameEmail($name, $email, $password);
+
+		$this->_addCaptchaAction('register');
+
+		return true;
+	}
+
+	protected function _registerNewUserNameEmail(string $name, string $email, string $password): void
+	{
+		$transaction = F::$mysql->beginTransaction();
+
+		$userId = $this->_saveUserNameEmailToDatabase($name, $email, $password);
+
+		$token = $this->_createToken($userId, 'register');
+
+		$transaction->commit();
+
+		$this->_sendMailRegister($name, $email, $token);
+	}
+
+	protected function _registerNewUserLoginEmail(string $login, string $email, string $password): void
+	{
+		$transaction = F::$mysql->beginTransaction();
+
+		$userId = $this->_saveUserLoginEmailToDatabase($login, $email, $password);
+
+		$token = $this->_createToken($userId, 'register');
+
+		$transaction->commit();
+
+		$this->_sendMailRegister($login, $email, $token);
+	}
+
+	public function restore(string $email, ?string $captcha): bool
+	{
+		$email = remove_invisible_symbols($email);
+
+		if ($this->errors = $this->_validateCaptcha($captcha, 'restore') or $this->errors = $this->_validateRestore($email))
+		{
+			return false;
+		}
+
+		$this->_restorePassword($email);
+
+		return true;
+	}
+
+	protected function _validateRestore(string $email): array
+	{
+		$errors = $this->_validateEmail($email);
+
+		if (! $errors)
+		{
+			if (! $this->loadUserByEmail($email))
+			{
+				$errors['email'] = $this->_messages['email.not_found']; /// такой email не зарегистрирован
+			}
+		}
+
+		return $errors;
+	}
+
+	protected function _restorePassword(string $email): void
+	{
+		$user = $this->loadUserByEmail($email);
+
+		$transaction = F::$mysql->beginTransaction();
+
+		$token = $this->_createToken($user->id, 'restore');
+
+		$this->_sendMailRestore($email, $token);
+
+		$transaction->commit();
+	}
+
+	public function restoreConfirm(CModel $user, string $password, string $passwordRepeat): bool
+	{
+		if ($this->errors = $this->_validateRestoreConfirm($password, $passwordRepeat))
+		{
+			return false;
+		}
+
+		$this->_restoreConfirmComplete($user, $password);
+
+		return true;
+	}
+
+	protected function _validateRestoreConfirm(string $password, string $passwordRepeat): array
+	{
+		$errors = $this->_validatePassword($password);
+
+		if ($password != $passwordRepeat)
+		{
+			$errors['passwordRepeat'] = $this->_messages['password.not_match']; /// пароли не совпадают
+		}
+
+		return $errors;
+	}
+
+	protected function _restoreConfirmComplete(CModel $user, $password): void
+	{
+		$user->saveAttributes([
+			'password' => $this->encodePassword($password),
+		]);
+
+		$this->loginUser($user);
+	}
+
+	protected function _sendMailRegister(string $name, string $email, string $token): void
+	{
+		/// отправка юзеру письма о регистрации
+		/// определить в наследнике
+	}
+
+	protected function _sendMailRestore(string $email, string $token): void
+	{
+		/// отправка юзеру письма о восстановлении пароля
+		/// определить в наследнике
+	}
+
+	/// создание нового юзера (имя + email + пароль)
+	/// определить в наследнике
+	abstract protected function _saveUserNameEmailToDatabase(string $name, string $email, string $password): int;
+
+	/// создание нового юзера (логин + email + пароль)
+	/// определить в наследнике
+	abstract protected function _saveUserLoginEmailToDatabase(string $login, string $email, string $password): int;
+
+	/// токен для подтверждения регистрации или восстановления пароля
+	/// определить в наследнике
+	abstract protected function _createToken(int $userId, string $action): string;
+
+}
---
+++ myframework/auth/CAuthShort.php
@@ -0,0 +1,255 @@
+<?php
+namespace myframework;
+
+/**
+ * Только авторизация, без регистрации
+ */
+abstract class CAuthShort extends CSingleton
+{
+	const MIN_LOGIN_LENGTH = 3; /// минимальная длина логина
+
+	const MAX_LOGIN_LENGTH = 20; /// максимальная длина логина
+
+	const MAX_NAME_LENGTH = 100; /// максимальная длина имени юзера
+
+	const MAX_EMAIL_LENGTH = 100; /// максимальная длина email
+
+	const MIN_PASSWORD_LENGTH = 6; /// минимальная длина пароля
+
+	const MAX_PASSWORD_LENGTH = 64; /// максимальная длина пароля
+
+	const CAPTCHA_LOGIN_COUNT = 5; /// показывать капчу после N попыток логина [в течение некоторого интервала времени]
+
+	const CAPTCHA_REGISTER_COUNT = 5; /// показывать капчу после N регистраций [в течение некоторого интервала времени]
+
+	public array $errors = [];
+
+	protected CModel $_user;
+
+	protected bool $_useCaptcha = true;
+
+	protected array $_messages;
+
+	protected static string $_passwordSalt; /// определить в наследнике
+
+	protected static string $_tokenSalt; /// определить в наследнике
+
+	protected function __construct()
+	{
+		$this->_messages = (require_once F_ROOT . '/messages/ru.php')['auth'];
+	}
+
+	public function loginByEmail(string $email, string $password, ?string $captcha, bool $remember = true): bool
+	{
+		if ($this->errors = $this->_validateCaptcha($captcha, 'login'))
+		{
+			return false;
+		}
+
+		if ($this->errors = $this->_validatePassword($password) + $this->_validateEmail($email))
+		{
+			return false;
+		}
+
+		if (! ($user = $this->loadUserByEmail($email))) /// не нашли в базе юзера с таким email
+		{
+			$this->errors['email'] = $this->_messages['email.not_found'];
+
+			$this->_addCaptchaAction('login');
+
+			return false;
+		}
+		elseif ($user->password != $this->encodePassword($password)) /// юзера нашли, но пароль не совпал
+		{
+			$this->errors['password'] = $this->_messages['password.wrong'];
+
+			$this->_addCaptchaAction('login');
+
+			return false;
+		}
+
+		$this->loginUser($user);
+
+		return true;
+	}
+
+	public function loginUser($user)
+	{
+		$this->_user = $user;
+
+		foreach ($user as $key => $value) /// будут добавлены только публичные поля, которые были выбраны в селекте
+		{
+			F::$app->session->{'user_' . $key} = $value;
+		}
+	}
+
+	public function logout()
+	{
+		/// везде авторизация определяется исключительно по установленному user_id в сессии
+		/// установка user_id (и других полей user_*) происходит в loginUser чуть выше
+		F::$app->session->user_id = null;
+	}
+
+	abstract public function loadUserByEmail(string $email): ?CModel;
+
+	abstract public function loadUserByLogin(string $login): ?CModel;
+
+	protected function _validateName(string $name): array
+	{
+		$errors = [];
+
+		if (! $name)
+		{
+			$errors['name'] = $this->_messages['name.empty'];
+		}
+		elseif (! preg_match('|^[а-яё \-]+$|ui', $name)) /// разрешены русские буквы, пробел и дефис. имя не уникально, потому без цифр и иных символов
+		{
+			$errors['name'] = $this->_messages['name.wrong_symbols'];
+		}
+		elseif (mb_strlen($name) > self::MAX_NAME_LENGTH)
+		{
+			$errors['name'] = $this->_message('name.too_long', ['{num}' => self::MAX_NAME_LENGTH]);
+		}
+
+		return $errors;
+	}
+
+	protected function _validateEmail(string $email): array
+	{
+		$errors = [];
+
+		if (! $email)
+		{
+			$errors['email'] = $this->_messages['email.empty'];
+		}
+		elseif (! filter_var($email, FILTER_VALIDATE_EMAIL))
+		{
+			$errors['email'] = $this->_messages['email.wrong'];
+		}
+		elseif (mb_strlen($email) > self::MAX_EMAIL_LENGTH)
+		{
+			$errors['email'] = $this->_message('email.too_long', ['{num}' => self::MAX_EMAIL_LENGTH,]);
+		}
+
+		return $errors;
+	}
+
+	protected function _validatePassword(string $password): array
+	{
+		$errors = [];
+
+		if (! $password)
+		{
+			$errors['password'] = $this->_messages['password.empty'];
+		}
+		elseif (($len = mb_strlen($password)) < self::MIN_PASSWORD_LENGTH)
+		{
+			$errors['password'] = $this->_message('password.too_short', ['{num}' => self::MIN_PASSWORD_LENGTH,]);
+		}
+		elseif ($len > self::MAX_PASSWORD_LENGTH)
+		{
+			$errors['password'] = $this->_message('password.too_long', ['{num}' => self::MAX_PASSWORD_LENGTH,]);
+		}
+		elseif (ctype_digit($password))
+		{
+			$errors['password'] = $this->_messages['password.only_digits'];
+		}
+
+		return $errors;
+	}
+
+	protected function _validateCaptcha(?string $captcha, string $action = ''): array
+	{
+		$errors = [];
+
+		if ($this->_showCaptcha($action))
+		{
+			if (! $captcha)
+			{
+				$errors['captcha'] = $this->_messages['captcha.empty'];
+			}
+			elseif (! CCaptcha::checkCaptcha($captcha))
+			{
+				$errors['captcha'] = $this->_messages['captcha.wrong'];
+			}
+
+			if (! $errors) /// удачно введена капча
+			{
+				$this->_deleteAllCaptchaActions($action);
+			}
+		}
+
+		return $errors;
+	}
+
+	protected function _validateLogin(string $login): array
+	{
+		$errors = [];
+
+		if (! $login)
+		{
+			$errors['login'] = $this->_messages['login.empty'];
+		}
+		elseif (! preg_match('|^[\w\-]+$|', $login)) /// разрешены буквы, цифры, подчёркивание _ (входит в \w), минус
+		{
+			$errors['login'] = $this->_messages['login.wrong_symbols'];
+		}
+		elseif (($len = strlen($login)) < self::MIN_LOGIN_LENGTH)
+		{
+			$errors['login'] = $this->_message('login.too_short', ['{num}' => self::MIN_LOGIN_LENGTH]);
+		}
+		elseif ($len > self::MAX_LOGIN_LENGTH)
+		{
+			$errors['login'] = $this->_message('login.too_long', ['{num}' => self::MAX_LOGIN_LENGTH]);
+		}
+
+		return $errors;
+	}
+
+	protected function _showCaptcha(string $action): bool
+	{
+		if ($action == 'login')
+		{
+			return $this->_useCaptcha && (! self::CAPTCHA_LOGIN_COUNT || $this->_getCaptchaActionCount($action) >= self::CAPTCHA_LOGIN_COUNT);
+		}
+		elseif ($action == 'register')
+		{
+			return $this->_useCaptcha && (! self::CAPTCHA_REGISTER_COUNT || $this->_getCaptchaActionCount($action) >= self::CAPTCHA_REGISTER_COUNT);
+		}
+
+		return $this->_useCaptcha;
+	}
+
+	protected function _deleteAllCaptchaActions(string $action): void
+	{
+		/// обнулить счётчик попыток авотризации / регистрации
+		/// определить в наследнике
+	}
+
+	protected function _addCaptchaAction(string $action): void
+	{
+		/// счётчик попыток авторизации либо регистрации с этого ip
+		/// на каждую неудачную попытку для этого ip счётчик увеличивается на 1. при превышении некоторого порога вылазит капча
+		/// определить в наследнике
+	}
+
+	protected function _getCaptchaActionCount(string $action): int
+	{
+		/// достать текущее значение счётчика
+		/// определить в наследнике
+		return 0;
+	}
+
+	public static function encodePassword(string $password): string
+	{
+		return md5($password . self::$_passwordSalt);
+	}
+
+	protected function _message(string $key, array $params): string
+	{
+		$message = str_replace(array_keys($params), array_values($params), $this->_messages[$key]);
+
+		return $message;
+	}
+
+}
